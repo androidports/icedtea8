14064ba3e61c512c93ec31929db73e9df9f43123
Lillian Angel <langel@redhat.com>
2009-05-21 14:55:51 -0400

    2009-05-21  Lillian Angel  <langel@redhat.com>
            * .hgignore: Updated.

---
+++ ports/hotspot/src/share/vm/shark/sharkRuntime.cpp
@@ -0,0 +1,476 @@
+/*
+ * Copyright 1999-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2008, 2009 Red Hat, Inc.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ *
+ */
+
+#include "incls/_precompiled.incl"
+#include "incls/_sharkRuntime.cpp.incl"
+
+using namespace llvm;
+
+// VM calls
+Constant* SharkRuntime::_find_exception_handler;
+Constant* SharkRuntime::_monitorenter;
+Constant* SharkRuntime::_monitorexit;
+Constant* SharkRuntime::_new_instance;
+Constant* SharkRuntime::_newarray;
+Constant* SharkRuntime::_anewarray;
+Constant* SharkRuntime::_multianewarray;
+Constant* SharkRuntime::_register_finalizer;
+Constant* SharkRuntime::_safepoint;
+Constant* SharkRuntime::_throw_ArrayIndexOutOfBoundsException;
+Constant* SharkRuntime::_throw_NullPointerException;
+
+// Leaf calls
+Constant* SharkRuntime::_f2i;
+Constant* SharkRuntime::_f2l;
+Constant* SharkRuntime::_d2i;
+Constant* SharkRuntime::_d2l;
+
+// Non-VM calls
+Constant* SharkRuntime::_dump;
+Constant* SharkRuntime::_is_subtype_of;
+Constant* SharkRuntime::_should_not_reach_here;
+Constant* SharkRuntime::_unimplemented;
+Constant* SharkRuntime::_uncommon_trap;
+Constant* SharkRuntime::_current_time_millis;
+Constant* SharkRuntime::_fabs;
+Constant* SharkRuntime::_tan;
+Constant* SharkRuntime::_atan2;
+Constant* SharkRuntime::_unsafe_field_offset_to_byte_offset;
+
+extern jlong Unsafe_field_offset_to_byte_offset(jlong field_offset);
+
+void SharkRuntime::initialize(SharkBuilder* builder)
+{
+  // VM calls
+  std::vector<const Type*> params;
+  params.push_back(SharkType::thread_type());
+  params.push_back(PointerType::getUnqual(SharkType::jint_type()));
+  params.push_back(SharkType::jint_type());
+  _find_exception_handler = builder->make_function(
+    (intptr_t) find_exception_handler_C,
+    FunctionType::get(SharkType::jint_type(), params, false),
+    "SharkRuntime__find_exception_handler");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(PointerType::getUnqual(SharkType::monitor_type()));
+  _monitorenter = builder->make_function(
+    (intptr_t) monitorenter_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__monitorenter");
+  _monitorexit = builder->make_function(
+    (intptr_t) monitorexit_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__monitorexit");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::jint_type());
+  _new_instance = builder->make_function(
+    (intptr_t) new_instance_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__new_instance");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::jint_type());
+  params.push_back(SharkType::jint_type());
+  _newarray = builder->make_function(
+    (intptr_t) newarray_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__newarray");
+  _anewarray = builder->make_function(
+    (intptr_t) anewarray_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__anewarray");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::jint_type());
+  params.push_back(SharkType::jint_type());
+  params.push_back(PointerType::getUnqual(SharkType::jint_type()));
+  _multianewarray = builder->make_function(
+    (intptr_t) multianewarray_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__multianewarray");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::oop_type());
+  _register_finalizer = builder->make_function(
+    (intptr_t) register_finalizer_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__register_finalizer");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  _safepoint = builder->make_function(
+    (intptr_t) SafepointSynchronize::block,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SafepointSynchronize__block");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::intptr_type());
+  params.push_back(SharkType::jint_type());
+  params.push_back(SharkType::jint_type());
+  _throw_ArrayIndexOutOfBoundsException = builder->make_function(
+    (intptr_t) throw_ArrayIndexOutOfBoundsException_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__throw_ArrayIndexOutOfBoundsException");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::intptr_type());
+  params.push_back(SharkType::jint_type());
+  _throw_NullPointerException = builder->make_function(
+    (intptr_t) throw_NullPointerException_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__throw_NullPointerException");
+
+  // Leaf calls
+  params.clear();
+  params.push_back(SharkType::jfloat_type());
+  _f2i = builder->make_function(
+    (intptr_t) SharedRuntime::f2i,
+    FunctionType::get(SharkType::jint_type(), params, false),
+    "SharedRuntime__f2i");  
+  _f2l = builder->make_function(
+    (intptr_t) SharedRuntime::f2l,
+    FunctionType::get(SharkType::jlong_type(), params, false),
+    "SharedRuntime__f2l");  
+
+  params.clear();
+  params.push_back(SharkType::jdouble_type());
+  _d2i = builder->make_function(
+    (intptr_t) SharedRuntime::d2i,
+    FunctionType::get(SharkType::jint_type(), params, false),
+    "SharedRuntime__d2i");  
+  _d2l = builder->make_function(
+    (intptr_t) SharedRuntime::d2l,
+    FunctionType::get(SharkType::jlong_type(), params, false),
+    "SharedRuntime__d2l");  
+  
+  // Non-VM calls
+  params.clear();
+  params.push_back(SharkType::intptr_type());
+  params.push_back(SharkType::intptr_type());
+  _dump = builder->make_function(
+    (intptr_t) dump_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__dump");
+
+  params.clear();
+  params.push_back(SharkType::oop_type());
+  params.push_back(SharkType::oop_type());
+  assert(sizeof(bool) == 1, "fix this");
+  _is_subtype_of = builder->make_function(
+    (intptr_t) is_subtype_of_C,
+    FunctionType::get(Type::Int8Ty, params, false),
+    "SharkRuntime__is_subtype_of");
+
+  params.clear();
+  params.push_back(SharkType::intptr_type());
+  params.push_back(SharkType::jint_type());
+  _should_not_reach_here = builder->make_function(
+    (intptr_t) report_should_not_reach_here,
+    FunctionType::get(Type::VoidTy, params, false),
+    "report_should_not_reach_here");
+  _unimplemented = builder->make_function(
+    (intptr_t) report_unimplemented,
+    FunctionType::get(Type::VoidTy, params, false),
+    "report_unimplemented");
+
+  params.clear();
+  params.push_back(SharkType::thread_type());
+  params.push_back(SharkType::jint_type());
+  _uncommon_trap = builder->make_function(
+    (intptr_t) uncommon_trap_C,
+    FunctionType::get(Type::VoidTy, params, false),
+    "SharkRuntime__uncommon_trap");
+
+  params.clear();
+  _current_time_millis = builder->make_function(
+    (intptr_t) os::javaTimeMillis,
+    FunctionType::get(SharkType::jlong_type(), params, false),
+    "os__javaTimeMillis");
+
+  params.clear();
+  params.push_back(SharkType::jdouble_type());
+  _fabs = builder->make_function(
+    (intptr_t) ::fabs,
+    FunctionType::get(SharkType::jdouble_type(), params, false),
+    "fabs");
+  _tan = builder->make_function(
+    (intptr_t) ::tan,
+    FunctionType::get(SharkType::jdouble_type(), params, false),
+    "tan");
+  params.push_back(SharkType::jdouble_type());
+  _atan2 = builder->make_function(
+    (intptr_t) ::atan2,
+    FunctionType::get(SharkType::jdouble_type(), params, false),
+    "atan2");
+
+  params.clear();
+  params.push_back(SharkType::jlong_type());
+  _unsafe_field_offset_to_byte_offset = builder->make_function(
+    (intptr_t) Unsafe_field_offset_to_byte_offset,
+    FunctionType::get(SharkType::jlong_type(), params, false),
+    "Unsafe_field_offset_to_byte_offset");
+}
+
+JRT_ENTRY(int, SharkRuntime::find_exception_handler_C(JavaThread* thread,
+                                                      int*        indexes,
+                                                      int         num_indexes))
+{
+  constantPoolHandle pool(thread, method(thread)->constants());
+  KlassHandle exc_klass(thread, ((oop) tos_at(thread, 0))->klass());
+
+  for (int i = 0; i < num_indexes; i++) {
+    klassOop tmp = pool->klass_at(indexes[i], CHECK_0);
+    KlassHandle chk_klass(thread, tmp);
+
+    if (exc_klass() == chk_klass())
+      return i;
+
+    if (exc_klass()->klass_part()->is_subtype_of(chk_klass()))
+      return i;
+  }
+
+  return -1;
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::monitorenter_C(JavaThread*      thread,
+                                             BasicObjectLock* lock))
+{
+  if (PrintBiasedLockingStatistics)
+    Atomic::inc(BiasedLocking::slow_path_entry_count_addr());
+
+  Handle object(thread, lock->obj());  
+  assert(Universe::heap()->is_in_reserved_or_null(object()), "should be");
+  if (UseBiasedLocking) {
+    // Retry fast entry if bias is revoked to avoid unnecessary inflation
+    ObjectSynchronizer::fast_enter(object, lock->lock(), true, CHECK);
+  } else {
+    ObjectSynchronizer::slow_enter(object, lock->lock(), CHECK);
+  }
+  assert(Universe::heap()->is_in_reserved_or_null(lock->obj()), "should be");
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::monitorexit_C(JavaThread*      thread,
+                                            BasicObjectLock* lock))
+{
+  Handle object(thread, lock->obj());  
+  assert(Universe::heap()->is_in_reserved_or_null(object()), "should be");
+  if (lock == NULL || object()->is_unlocked()) {
+    THROW(vmSymbols::java_lang_IllegalMonitorStateException());
+  }
+  ObjectSynchronizer::slow_exit(object(), lock->lock(), thread);
+
+  // Free entry. This must be done here, since a pending exception
+  // might be installed on exit. If it is not cleared, the exception
+  // handling code will try to unlock the monitor again.
+  lock->set_obj(NULL); 
+}
+JRT_END
+  
+JRT_ENTRY(void, SharkRuntime::new_instance_C(JavaThread* thread, int index))
+{
+  klassOop k_oop = method(thread)->constants()->klass_at(index, CHECK);
+  instanceKlassHandle klass(THREAD, k_oop);
+  
+  // Make sure we are not instantiating an abstract klass
+  klass->check_valid_for_instantiation(true, CHECK);
+
+  // Make sure klass is initialized
+  klass->initialize(CHECK);    
+
+  // At this point the class may not be fully initialized
+  // because of recursive initialization. If it is fully
+  // initialized & has_finalized is not set, we rewrite
+  // it into its fast version (Note: no locking is needed
+  // here since this is an atomic byte write and can be
+  // done more than once).
+  //
+  // Note: In case of classes with has_finalized we don't
+  //       rewrite since that saves us an extra check in
+  //       the fast version which then would call the
+  //       slow version anyway (and do a call back into
+  //       Java).
+  //       If we have a breakpoint, then we don't rewrite
+  //       because the _breakpoint bytecode would be lost.
+  oop obj = klass->allocate_instance(CHECK);
+  thread->set_vm_result(obj);  
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::newarray_C(JavaThread* thread,
+                                         BasicType   type,
+                                         int         size))
+{
+  oop obj = oopFactory::new_typeArray(type, size, CHECK);
+  thread->set_vm_result(obj);
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::anewarray_C(JavaThread* thread,
+                                          int         index,
+                                          int         size))
+{
+  klassOop klass = method(thread)->constants()->klass_at(index, CHECK);
+  objArrayOop obj = oopFactory::new_objArray(klass, size, CHECK);
+  thread->set_vm_result(obj);
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::multianewarray_C(JavaThread* thread,
+                                               int         index,
+                                               int         ndims,
+                                               int*        dims))
+{
+  klassOop klass = method(thread)->constants()->klass_at(index, CHECK);
+  oop obj = arrayKlass::cast(klass)->multi_allocate(ndims, dims, CHECK);
+  thread->set_vm_result(obj);
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::register_finalizer_C(JavaThread* thread,
+                                                   oop         object))
+{
+  assert(object->is_oop(), "should be");
+  assert(object->klass()->klass_part()->has_finalizer(), "should have");
+  instanceKlass::register_finalizer(instanceOop(object), CHECK);
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::throw_ArrayIndexOutOfBoundsException_C(
+                                                     JavaThread* thread,
+                                                     const char* file,
+                                                     int         line,
+                                                     int         index))
+{
+  char msg[jintAsStringSize];
+  snprintf(msg, sizeof(msg), "%d", index);
+  Exceptions::_throw_msg(
+    thread, file, line, 
+    vmSymbols::java_lang_ArrayIndexOutOfBoundsException(),
+    msg);
+}
+JRT_END
+
+JRT_ENTRY(void, SharkRuntime::throw_NullPointerException_C(JavaThread* thread,
+                                                           const char* file,
+                                                           int         line))
+{
+  Exceptions::_throw_msg(
+    thread, file, line, 
+    vmSymbols::java_lang_NullPointerException(),
+    "");
+}
+JRT_END
+
+// Non-VM calls
+// Nothing in these must ever GC!
+
+void SharkRuntime::dump_C(const char *name, intptr_t value)
+{
+  oop valueOop = (oop) value;
+  tty->print("%s = ", name);
+  if (valueOop->is_oop(true))
+    valueOop->print_on(tty);
+  else if (value >= ' ' && value <= '~')
+    tty->print("'%c' (%d)", value, value);
+  else
+    tty->print("%p", value);
+  tty->print_cr("");
+}
+
+bool SharkRuntime::is_subtype_of_C(klassOop check_klass, klassOop object_klass)
+{
+  return object_klass->klass_part()->is_subtype_of(check_klass);
+}
+
+void SharkRuntime::uncommon_trap_C(JavaThread* thread, int trap_request)
+{
+  // In C2, uncommon_trap_blob creates a frame, so all the various
+  // deoptimization functions expect to find the frame of the method
+  // being deopted one frame down on the stack.  Create a dummy frame
+  // to mirror this.
+  ZeroStack *stack = thread->zero_stack();
+  thread->push_zero_frame(DeoptimizerFrame::build(stack));
+
+  // Initiate the trap
+  thread->set_last_Java_frame();
+  Deoptimization::UnrollBlock *urb =
+    Deoptimization::uncommon_trap(thread, trap_request);
+  thread->reset_last_Java_frame();
+
+  // Pop our dummy frame and the frame being deoptimized
+  thread->pop_zero_frame();
+  thread->pop_zero_frame();
+
+  // Push skeleton frames
+  int number_of_frames = urb->number_of_frames();
+  for (int i = 0; i < number_of_frames; i++) {
+    intptr_t size = urb->frame_sizes()[i];
+    thread->push_zero_frame(InterpreterFrame::build(stack, size));
+  }
+
+  // Push another dummy frame
+  thread->push_zero_frame(DeoptimizerFrame::build(stack));
+  
+  // Fill in the skeleton frames
+  thread->set_last_Java_frame();
+  Deoptimization::unpack_frames(thread, Deoptimization::Unpack_uncommon_trap);
+  thread->reset_last_Java_frame();
+
+  // Pop our dummy frame
+  thread->pop_zero_frame();
+
+  // Jump into the interpreter  
+#ifdef CC_INTERP
+  CppInterpreter::main_loop(number_of_frames - 1, thread);
+#else
+  Unimplemented();
+#endif // CC_INTERP
+}
+
+DeoptimizerFrame* DeoptimizerFrame::build(ZeroStack* stack)
+{
+  if (header_words > stack->available_words()) {
+    Unimplemented();
+  }
+
+  stack->push(0); // next_frame, filled in later
+  intptr_t *fp = stack->sp();
+  assert(fp - stack->sp() == next_frame_off, "should be");
+
+  stack->push(DEOPTIMIZER_FRAME);
+  assert(fp - stack->sp() == frame_type_off, "should be");
+
+  return (DeoptimizerFrame *) fp;
+}
---
+++ ports/hotspot/src/share/vm/shark/sharkTopLevelBlock.hpp
@@ -0,0 +1,385 @@
+/*
+ * Copyright 1999-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2008, 2009 Red Hat, Inc.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ *
+ */
+
+class SharkTopLevelBlock : public SharkBlock {
+ public:
+  SharkTopLevelBlock(SharkFunction* function, ciTypeFlow::Block* ciblock)
+    : SharkBlock(function->builder(),
+                 function->target(),
+                 function->iter(),
+                 function->thread()),
+      _function(function),
+      _ciblock(ciblock),
+      _entered(false),
+      _has_trap(false),
+      _needs_phis(false),
+      _entry_state(NULL),
+      _entry_block(NULL) {}
+
+ private:
+  SharkFunction*     _function;
+  ciTypeFlow::Block* _ciblock;
+
+ public:
+  SharkFunction* function() const
+  {
+    return _function;
+  }
+  ciTypeFlow::Block* ciblock() const
+  {
+    return _ciblock;
+  }
+
+  // Typeflow properties
+ public:
+  int index() const
+  {
+    return ciblock()->pre_order();
+  }
+  bool is_private_copy() const
+  {
+    return ciblock()->is_backedge_copy();
+  }
+  int stack_depth_at_entry() const
+  {
+    return ciblock()->stack_size();
+  }
+  ciType* local_type_at_entry(int index) const
+  {
+    return ciblock()->local_type_at(index);
+  }
+  ciType* stack_type_at_entry(int slot) const
+  {
+    return ciblock()->stack_type_at(slot);
+  }  
+  int start() const
+  {
+    return ciblock()->start();
+  }
+  int limit() const
+  {
+    return ciblock()->limit();
+  }
+  bool falls_through() const
+  {
+    return ciblock()->control() == ciBlock::fall_through_bci;
+  }
+  int num_exceptions() const
+  {
+    return ciblock()->exceptions()->length();
+  }
+  SharkTopLevelBlock* exception(int index) const
+  {
+    return function()->block(ciblock()->exceptions()->at(index)->pre_order());
+  }
+  int num_successors() const
+  {
+    return ciblock()->successors()->length();
+  }
+  SharkTopLevelBlock* successor(int index) const
+  {
+    return function()->block(ciblock()->successors()->at(index)->pre_order());
+  }
+  SharkTopLevelBlock* bci_successor(int bci) const;
+
+  // Traps
+ private:
+  bool _has_trap;
+  int  _trap_request;
+  int  _trap_bci;
+
+  void set_trap(int trap_request, int trap_bci)
+  {
+    assert(!has_trap(), "shouldn't have");
+    _has_trap     = true;
+    _trap_request = trap_request;
+    _trap_bci     = trap_bci;
+  }
+
+ private:
+  bool has_trap()
+  {
+    return _has_trap;
+  }
+  int trap_request()
+  {
+    assert(has_trap(), "should have");
+    return _trap_request;
+  }
+  int trap_bci()
+  {
+    assert(has_trap(), "should have");
+    return _trap_bci;
+  }
+
+ private:
+  void scan_for_traps();
+
+  // Entry state
+ private:
+  bool _entered;
+  bool _needs_phis;
+
+ public:
+  bool entered() const
+  {
+    return _entered;
+  }
+  bool needs_phis() const
+  {
+    return _needs_phis;
+  }
+
+ private:
+  void enter(SharkTopLevelBlock* predecessor, bool is_exception);
+
+ public:
+  void enter()
+  {
+    enter(NULL, false);
+  }
+
+ private:
+  SharkState* _entry_state;
+
+ private:
+  SharkState* entry_state();
+
+ private:
+  llvm::BasicBlock* _entry_block;
+
+ public:
+  llvm::BasicBlock* entry_block() const
+  {
+    return _entry_block;
+  }
+
+ public:
+  void initialize();
+
+ public:
+  void add_incoming(SharkState* incoming_state);
+
+  // Method
+ public:
+  llvm::Value* method()
+  {
+    return current_state()->method();
+  }
+
+  // Code generation
+ public:
+  void emit_IR();
+
+  // Helpers
+ private:
+  llvm::Value* lookup_for_ldc();
+  llvm::Value* lookup_for_field_access();
+  void do_branch(int successor_index);
+
+  // Zero checks
+ private:
+  void do_zero_check(SharkValue* value);
+  void zero_check_value(SharkValue* value, llvm::BasicBlock* continue_block);
+
+ public:
+  void do_deferred_zero_check(SharkValue*       value,
+                              int               bci,
+                              SharkState*       saved_state,
+                              llvm::BasicBlock* continue_block);
+
+  // VM calls
+ private:
+  llvm::CallInst* call_vm_nocheck(llvm::Constant* callee,
+                                  llvm::Value**   args_start,
+                                  llvm::Value**   args_end)
+  {
+    current_state()->decache_for_VM_call();
+    function()->set_last_Java_frame();
+    llvm::CallInst *res = builder()->CreateCall(callee, args_start, args_end);
+    function()->reset_last_Java_frame();
+    current_state()->cache_after_VM_call();
+    return res;
+  }
+
+  llvm::CallInst* call_vm(llvm::Constant* callee,
+                          llvm::Value**   args_start,
+                          llvm::Value**   args_end)
+  {
+    llvm::CallInst* res = call_vm_nocheck(callee, args_start, args_end);
+    check_pending_exception();
+    return res;
+  }
+
+ public:
+  llvm::CallInst* call_vm(llvm::Constant* callee)
+  {
+    llvm::Value *args[] = {thread()};
+    return call_vm(callee, args, args + 1);
+  }
+  llvm::CallInst* call_vm(llvm::Constant* callee,
+                          llvm::Value*    arg1)
+  {
+    llvm::Value *args[] = {thread(), arg1};
+    return call_vm(callee, args, args + 2);
+  }
+  llvm::CallInst* call_vm(llvm::Constant* callee,
+                          llvm::Value*    arg1,
+                          llvm::Value*    arg2)
+  {
+    llvm::Value *args[] = {thread(), arg1, arg2};
+    return call_vm(callee, args, args + 3);
+  }
+  llvm::CallInst* call_vm(llvm::Constant* callee,
+                          llvm::Value*    arg1,
+                          llvm::Value*    arg2,
+                          llvm::Value*    arg3)
+  {
+    llvm::Value *args[] = {thread(), arg1, arg2, arg3};
+    return call_vm(callee, args, args + 4);
+  }
+
+  llvm::CallInst* call_vm_nocheck(llvm::Constant* callee)
+  {
+    llvm::Value *args[] = {thread()};
+    return call_vm_nocheck(callee, args, args + 1);
+  }
+  llvm::CallInst* call_vm_nocheck(llvm::Constant* callee,
+                                  llvm::Value*    arg1)
+  {
+    llvm::Value *args[] = {thread(), arg1};
+    return call_vm_nocheck(callee, args, args + 2);
+  }
+  llvm::CallInst* call_vm_nocheck(llvm::Constant* callee,
+                                  llvm::Value*    arg1,
+                                  llvm::Value*    arg2)
+  {
+    llvm::Value *args[] = {thread(), arg1, arg2};
+    return call_vm_nocheck(callee, args, args + 3);
+  }
+  llvm::CallInst* call_vm_nocheck(llvm::Constant* callee,
+                                  llvm::Value*    arg1,
+                                  llvm::Value*    arg2,
+                                  llvm::Value*    arg3)
+  {
+    llvm::Value *args[] = {thread(), arg1, arg2, arg3};
+    return call_vm_nocheck(callee, args, args + 4);
+  }
+
+  // Whole-method synchronization
+ public:
+  void acquire_method_lock();  
+  void release_method_lock();  
+
+  // Error checking
+ private:
+  void check_bounds(SharkValue* array, SharkValue* index);
+  void check_pending_exception(bool attempt_catch = true);
+  void handle_exception(llvm::Value* exception, bool attempt_catch = true);
+
+  // Safepoints
+ private:
+  void add_safepoint();
+
+  // Traps
+ private:
+  void do_trap(int trap_request);
+
+  // Returns
+ private:
+  void call_register_finalizer(llvm::Value* receiver);
+  void handle_return(BasicType type, llvm::Value* exception);
+  void release_locked_monitors();
+
+  // arraylength
+ private:
+  void do_arraylength();
+
+  // *aload and *astore
+ private:
+  void do_aload(BasicType basic_type);
+  void do_astore(BasicType basic_type);
+
+  // *return and athrow
+ private:
+  void do_return(BasicType type);
+  void do_athrow();
+
+  // goto*
+ private:
+  void do_goto();
+
+  // jsr* and ret
+ private:
+  void do_jsr();
+  void do_ret();
+
+  // if*
+ private:
+  void do_if(llvm::ICmpInst::Predicate p, SharkValue* b, SharkValue* a);
+
+  // tableswitch and lookupswitch
+ private:
+  void do_switch();
+
+  // invoke*
+ private:
+  enum CallType {
+    CALL_DIRECT,
+    CALL_VIRTUAL,
+    CALL_INTERFACE
+  };
+  CallType get_call_type(ciMethod* method);
+  llvm::Value* get_callee(CallType    call_type,
+                          ciMethod*   method,
+                          SharkValue* receiver);
+
+  llvm::Value* get_direct_callee(ciMethod* method);
+  llvm::Value* get_virtual_callee(SharkValue* receiver, ciMethod* method);
+
+  llvm::Value* get_virtual_callee(llvm::Value* cache, SharkValue* receiver);
+  llvm::Value* get_interface_callee(SharkValue* receiver);
+
+  void do_call();
+
+  // checkcast and instanceof
+ private:
+  void do_full_instance_check();
+  void do_trapping_instance_check();
+
+  void do_instance_check();
+
+  // new and *newarray
+ private:
+  void do_new();
+  void do_newarray();
+  void do_anewarray();
+  void do_multianewarray();
+
+  // monitorenter and monitorexit
+ private:
+  void do_monitorenter();
+  void do_monitorexit();
+};
