4856a0ab301032b99eabc44ac581fe37d0354e5b
Andrew John Hughes <ahughes@redhat.com>
2009-05-22 23:35:26 +0100

    Fix the Shark build by defining InlineSmallCode.
    2009-05-22  Andrew John Hughes  <ahughes@redhat.com>
    
    	* ports/hotspot/src/cpu/zero/vm/globals_zero.hpp:
    	Define InlineSmallCode, using x86 value to begin with.

---
+++ ports/hotspot/src/share/vm/shark/sharkBuilder.hpp
@@ -0,0 +1,363 @@
+/*
+ * Copyright 1999-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2008, 2009 Red Hat, Inc.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ *
+ */
+
+class SharkBuilder : public llvm::IRBuilder<> {
+ public:
+  SharkBuilder(SharkCompiler* compiler);
+
+ private:
+  SharkCompiler* _compiler;
+
+ public:
+  SharkCompiler* compiler() const
+  {
+    return _compiler;
+  }
+
+ private:
+  llvm::Module* module() const
+  {
+    return compiler()->module();
+  }
+  llvm::ExecutionEngine* execution_engine() const
+  {
+    return compiler()->execution_engine();
+  }
+
+  // Function creation
+ public:
+  llvm::Function *CreateFunction(const char *name = "func");
+
+  // Helpers for creating basic blocks
+  // NB don't use unless SharkFunction::CreateBlock is unavailable
+ public:
+  llvm::BasicBlock* GetBlockInsertionPoint() const
+  {
+    llvm::BasicBlock *cur = GetInsertBlock();
+
+    // BasicBlock::Create takes an insertBefore argument, so
+    // we need to find the block _after_ the current block
+    llvm::Function::iterator iter = cur->getParent()->begin();
+    llvm::Function::iterator end  = cur->getParent()->end();
+    while (iter != end) {
+      iter++;
+      if (&*iter == cur) {
+        iter++;
+        break;
+      }
+    }
+
+    if (iter == end)
+      return NULL;
+    else
+      return iter;
+  }
+  llvm::BasicBlock* CreateBlock(llvm::BasicBlock* ip, const char* name="") const
+  {
+    return llvm::BasicBlock::Create(name, GetInsertBlock()->getParent(), ip);
+  }
+  
+  // Helpers for accessing structures and arrays
+ public:
+  llvm::Value* CreateAddressOfStructEntry(llvm::Value* base,
+                                          ByteSize offset,
+                                          const llvm::Type* type,
+                                          const char *name = "")
+  {
+    return CreateBitCast(CreateStructGEP(base, in_bytes(offset)), type, name);
+  }
+
+  llvm::LoadInst* CreateValueOfStructEntry(llvm::Value* base,
+                                           ByteSize offset,
+                                           const llvm::Type* type,
+                                           const char *name = "")
+  {
+    return CreateLoad(
+      CreateAddressOfStructEntry(
+        base, offset, llvm::PointerType::getUnqual(type)),
+      name);
+  }
+
+  llvm::LoadInst* CreateArrayLength(llvm::Value* array)
+  {
+    return CreateValueOfStructEntry(
+      array, in_ByteSize(arrayOopDesc::length_offset_in_bytes()),
+      SharkType::jint_type(), "length");
+  }
+
+  llvm::Value* CreateArrayAddress(llvm::Value*      arrayoop,
+                                  const llvm::Type* element_type,
+                                  int               element_bytes,
+                                  ByteSize          base_offset,
+                                  llvm::Value*      index,
+                                  const char*       name = "")
+  {
+    llvm::Value* offset = CreateIntCast(index, SharkType::intptr_type(), false);
+    if (element_bytes != 1)
+      offset = CreateShl(
+        offset,
+        LLVMValue::intptr_constant(exact_log2(element_bytes)));
+    offset = CreateAdd(
+      LLVMValue::intptr_constant(in_bytes(base_offset)), offset);
+
+    return CreateIntToPtr(
+      CreateAdd(CreatePtrToInt(arrayoop, SharkType::intptr_type()), offset),
+      llvm::PointerType::getUnqual(element_type),
+      name);
+  }
+
+  llvm::Value* CreateArrayAddress(llvm::Value* arrayoop,
+                                  BasicType    basic_type,
+                                  ByteSize     base_offset,
+                                  llvm::Value* index,
+                                  const char*  name = "")
+  {
+    return CreateArrayAddress(
+      arrayoop,
+      SharkType::to_arrayType(basic_type),
+      type2aelembytes(basic_type),
+      base_offset, index, name);
+  }
+
+  llvm::Value* CreateArrayAddress(llvm::Value* arrayoop,
+                                  BasicType    basic_type,
+                                  llvm::Value* index,
+                                  const char*  name = "")
+  {
+    return CreateArrayAddress(
+      arrayoop, basic_type,
+      in_ByteSize(arrayOopDesc::base_offset_in_bytes(basic_type)),
+      index, name);
+  }
+
+  // Helper for making function pointers
+ public:
+  llvm::Constant* make_function(intptr_t                  addr,
+                                const llvm::FunctionType* sig,
+                                const char*               name);
+
+  llvm::Constant* pointer_constant(const void *ptr)
+  {
+    // Create a pointer constant that points at PTR.  We do this by
+    // creating a GlobalVariable mapped at PTR.  This is a workaround
+    // for http://www.llvm.org/bugs/show_bug.cgi?id=2920
+
+    using namespace llvm;
+
+    // This might be useful but it returns a const pointer that can't
+    // be used for anything.  Go figure...
+//     {
+//       const GlobalValue *value
+// 	= execution_engine()->getGlobalValueAtAddress(const_cast<void*>(ptr));
+//       if (value)
+// 	return ConstantExpr::getPtrToInt(value, SharkType::intptr_type());
+//     }
+
+    char name[128];
+    snprintf(name, sizeof name - 1, "pointer_constant_%p", ptr);
+
+    GlobalVariable *value = new GlobalVariable(SharkType::intptr_type(),
+      false, GlobalValue::ExternalLinkage,
+      NULL, name, module());
+    execution_engine()->addGlobalMapping(value, const_cast<void*>(ptr));
+
+    return ConstantExpr::getPtrToInt(value, SharkType::intptr_type());
+  }
+
+  // Helper for making pointers
+ public:
+  llvm::Constant* make_pointer(intptr_t addr, const llvm::Type* type)
+  {
+    return llvm::ConstantExpr::getIntToPtr(
+      LLVMValue::intptr_constant(addr),
+      llvm::PointerType::getUnqual(type));
+  }
+
+  // External functions (and intrinsics)
+ private:
+  llvm::Constant* _llvm_cmpxchg_int_fn;
+  llvm::Constant* _llvm_cmpxchg_ptr_fn;  
+  llvm::Constant* _llvm_memory_barrier_fn;
+  llvm::Constant* _llvm_memset_fn;  
+  llvm::Constant* _llvm_sin_fn;
+  llvm::Constant* _llvm_cos_fn;
+  llvm::Constant* _llvm_sqrt_fn;
+  llvm::Constant* _llvm_log_fn;
+  llvm::Constant* _llvm_log10_fn;
+  llvm::Constant* _llvm_pow_fn;
+  llvm::Constant* _llvm_exp_fn;
+
+  void set_llvm_cmpxchg_int_fn(llvm::Constant* llvm_cmpxchg_int_fn)
+  {
+    _llvm_cmpxchg_int_fn = llvm_cmpxchg_int_fn;
+  }
+  void set_llvm_cmpxchg_ptr_fn(llvm::Constant* llvm_cmpxchg_ptr_fn)
+  {
+    _llvm_cmpxchg_ptr_fn = llvm_cmpxchg_ptr_fn;
+  }
+  void set_llvm_memory_barrier_fn(llvm::Constant* llvm_memory_barrier_fn)
+  {
+    _llvm_memory_barrier_fn = llvm_memory_barrier_fn;
+  }
+  void set_llvm_memset_fn(llvm::Constant* llvm_memset_fn)
+  {
+    _llvm_memset_fn = llvm_memset_fn;
+  }
+  void set_llvm_sin_fn(llvm::Constant* llvm_sin_fn)
+  {
+    _llvm_sin_fn = llvm_sin_fn;
+  }
+  void set_llvm_cos_fn(llvm::Constant* llvm_cos_fn)
+  {
+    _llvm_cos_fn = llvm_cos_fn;
+  }
+  void set_llvm_sqrt_fn(llvm::Constant* llvm_sqrt_fn)
+  {
+    _llvm_sqrt_fn = llvm_sqrt_fn;
+  }
+  void set_llvm_log_fn(llvm::Constant* llvm_log_fn)
+  {
+    _llvm_log_fn = llvm_log_fn;
+  }
+  void set_llvm_log10_fn(llvm::Constant* llvm_log10_fn)
+  {
+    _llvm_log10_fn = llvm_log10_fn;
+  }
+  void set_llvm_pow_fn(llvm::Constant* llvm_pow_fn)
+  {
+    _llvm_pow_fn = llvm_pow_fn;
+  }
+  void set_llvm_exp_fn(llvm::Constant* llvm_exp_fn)
+  {
+    _llvm_exp_fn = llvm_exp_fn;
+  }
+
+  void init_external_functions();
+
+ protected:
+  llvm::Constant* llvm_cmpxchg_int_fn() const
+  {
+    return _llvm_cmpxchg_int_fn;
+  }
+  llvm::Constant* llvm_cmpxchg_ptr_fn() const
+  {
+    return _llvm_cmpxchg_ptr_fn;
+  }
+  llvm::Constant* llvm_memory_barrier_fn() const
+  {
+    return _llvm_memory_barrier_fn;
+  }
+  llvm::Constant* llvm_memset_fn() const
+  {
+    return _llvm_memset_fn;
+  }
+
+ public:
+  llvm::Constant* llvm_sin_fn() const
+  {
+    return _llvm_sin_fn;
+  }
+  llvm::Constant* llvm_cos_fn() const
+  {
+    return _llvm_cos_fn;
+  }
+  llvm::Constant* llvm_sqrt_fn() const
+  {
+    return _llvm_sqrt_fn;
+  }
+  llvm::Constant* llvm_log_fn() const
+  {
+    return _llvm_log_fn;
+  }
+  llvm::Constant* llvm_log10_fn() const
+  {
+    return _llvm_log10_fn;
+  }
+  llvm::Constant* llvm_pow_fn() const
+  {
+    return _llvm_pow_fn;
+  }
+  llvm::Constant* llvm_exp_fn() const
+  {
+    return _llvm_exp_fn;
+  }
+
+ public:
+  llvm::CallInst* CreateDump(llvm::Value* value);
+  llvm::CallInst* CreateMemset(llvm::Value* dst,
+                               llvm::Value* value,
+                               llvm::Value* len,
+                               llvm::Value* align);
+  llvm::CallInst* CreateCmpxchgInt(llvm::Value* exchange_value,
+                                    llvm::Value* dst,
+                                    llvm::Value* compare_value);
+  llvm::CallInst* CreateCmpxchgPtr(llvm::Value* exchange_value,
+                                   llvm::Value* dst,
+                                   llvm::Value* compare_value);
+  llvm::CallInst* CreateShouldNotReachHere(const char* file, int line);  
+  llvm::CallInst* CreateUnimplemented(const char* file, int line);  
+
+  // HotSpot memory barriers
+ public:
+  void CreateUpdateBarrierSet(BarrierSet* bs, llvm::Value* field)
+  {
+    if (bs->kind() != BarrierSet::CardTableModRef) {
+      Unimplemented();
+    }
+
+    CreateStore(
+      LLVMValue::jbyte_constant(CardTableModRefBS::dirty_card),
+      CreateIntToPtr(
+        CreateAdd(
+          pointer_constant(((CardTableModRefBS *) bs)->byte_map_base),
+          CreateLShr(
+            CreatePtrToInt(field, SharkType::intptr_type()),
+            LLVMValue::intptr_constant(CardTableModRefBS::card_shift))),
+        llvm::PointerType::getUnqual(SharkType::jbyte_type())));
+  }
+
+  // Hardware memory barriers
+ public:
+  enum BarrierFlags {
+    BARRIER_LOADLOAD   = 1,
+    BARRIER_LOADSTORE  = 2,
+    BARRIER_STORELOAD  = 4,
+    BARRIER_STORESTORE = 8
+  };
+
+ public:
+  llvm::CallInst* CreateMemoryBarrier(BarrierFlags flags);
+
+  // Alignment
+ public:
+  llvm::Value* CreateAlign(llvm::Value* x, intptr_t s, const char* name = "")
+  {
+    assert(is_power_of_2(s), "should be");
+    return CreateAnd(
+      CreateAdd(x, LLVMValue::intptr_constant(s - 1)),
+      LLVMValue::intptr_constant(~(s - 1)),
+      name);
+  }
+};
---
+++ ports/hotspot/src/share/vm/shark/sharkFunction.hpp
@@ -0,0 +1,355 @@
+/*
+ * Copyright 1999-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2008, 2009 Red Hat, Inc.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
+ * CA 95054 USA or visit www.sun.com if you need additional information or
+ * have any questions.
+ *
+ */
+
+class SharkMonitor;
+class SharkTopLevelBlock;
+
+class DeferredZeroCheck;
+
+class SharkFunction : public StackObj {
+ public:
+  SharkFunction(SharkCompiler*    compiler,
+                const char*       name,
+                ciTypeFlow*       flow,
+                ciBytecodeStream* iter,
+                MacroAssembler*   masm)
+    : _compiler(compiler),
+      _name(name),
+      _flow(flow),
+      _iter(iter),
+      _masm(masm)
+  { initialize(); }
+
+ private:
+  void initialize();
+
+ private:
+  SharkCompiler*                    _compiler;
+  const char*                       _name;
+  ciTypeFlow*                       _flow;
+  ciBytecodeStream*                 _iter;
+  MacroAssembler*                   _masm;
+  llvm::Function*                   _function;
+  SharkTopLevelBlock**              _blocks;
+  llvm::Value*                      _base_pc;
+  llvm::Value*                      _thread;
+  int                               _monitor_count;
+  GrowableArray<DeferredZeroCheck*> _deferred_zero_checks;
+
+ public:  
+  SharkCompiler* compiler() const
+  {
+    return _compiler;
+  }
+  const char* name() const
+  {
+    return _name;
+  }
+  ciTypeFlow* flow() const
+  {
+    return _flow;
+  }
+  ciBytecodeStream* iter() const
+  {
+    return _iter;
+  }
+  MacroAssembler* masm() const
+  {
+    return _masm;
+  }
+  llvm::Function* function() const
+  {
+    return _function;
+  }
+  SharkTopLevelBlock* block(int i) const
+  {
+    return _blocks[i];
+  }
+  llvm::Value* base_pc() const
+  {
+    return _base_pc;
+  }
+  llvm::Value* thread() const
+  {
+    return _thread;
+  }
+  int monitor_count() const
+  {
+    return _monitor_count;
+  }
+  GrowableArray<DeferredZeroCheck*>* deferred_zero_checks()
+  {
+    return &_deferred_zero_checks;
+  }
+
+ public:
+  SharkBuilder* builder() const
+  {
+    return compiler()->builder();
+  }
+  int arg_size() const
+  {
+    return target()->arg_size();
+  }
+  int block_count() const
+  {
+    return flow()->block_count();
+  }
+  DebugInformationRecorder *debug_info() const
+  {
+    return env()->debug_info();
+  }
+  ciEnv* env() const
+  {
+    return flow()->env();
+  }
+  int max_locals() const
+  {
+    return flow()->max_locals();
+  }
+  int max_stack() const
+  {
+    return flow()->max_stack();
+  }
+  ciMethod* target() const
+  {
+    return flow()->method();
+  }
+
+  // Block management
+ private:
+  llvm::BasicBlock* _block_insertion_point;
+
+  void set_block_insertion_point(llvm::BasicBlock* block_insertion_point)
+  {
+    _block_insertion_point = block_insertion_point;
+  }
+  llvm::BasicBlock* block_insertion_point() const
+  {
+    return _block_insertion_point;
+  }
+
+ public:
+  llvm::BasicBlock* CreateBlock(const char* name = "") const
+  {
+    return llvm::BasicBlock::Create(name, function(), block_insertion_point());
+  }
+
+  // Stack management
+ private:
+  llvm::Value* _zero_stack_base;
+  llvm::Value* _zero_stack_pointer_addr;
+  llvm::Value* _zero_frame_pointer_addr;
+
+ private:
+  llvm::Value* zero_stack_base() const 
+  {
+    return _zero_stack_base;
+  }
+  llvm::Value* zero_stack_pointer_addr() const 
+  {
+    return _zero_stack_pointer_addr;
+  }
+  llvm::Value* zero_frame_pointer_addr() const 
+  {
+    return _zero_frame_pointer_addr;
+  }
+
+ private:
+  void CreateInitZeroStack();
+
+ public:
+  llvm::LoadInst* CreateLoadZeroStackPointer(const char *name = "")
+  {
+    return builder()->CreateLoad(zero_stack_pointer_addr(), name);
+  }
+  llvm::StoreInst* CreateStoreZeroStackPointer(llvm::Value* value)
+  {
+    return builder()->CreateStore(value, zero_stack_pointer_addr());
+  }
+
+ private:
+  llvm::LoadInst* CreateLoadZeroFramePointer(const char *name = "")
+  {
+    return builder()->CreateLoad(zero_frame_pointer_addr(), name);
+  }
+  llvm::StoreInst* CreateStoreZeroFramePointer(llvm::Value* value)
+  {
+    return builder()->CreateStore(value, zero_frame_pointer_addr());
+  }
+
+ private:
+  void CreateStackOverflowCheck(llvm::Value* sp);
+
+ public:
+  void CreatePushFrame(llvm::Value* fp);
+  llvm::Value* CreatePopFrame(int result_slots);
+
+  // Frame management
+ private:
+  llvm::Value* _frame;
+
+ public:
+  llvm::Value* CreateAddressOfFrameEntry(int               offset,
+                                         const llvm::Type* type = NULL,
+                                         const char*       name = "") const;
+ public:
+  llvm::Value* exception_slot() const
+  {
+    return CreateAddressOfFrameEntry(
+      exception_slot_offset(),
+      SharkType::oop_type(),
+      "exception_slot");
+  }
+  llvm::Value* monitors_slots() const
+  {
+    return CreateAddressOfFrameEntry(
+      monitors_slots_offset(),
+      llvm::ArrayType::get(SharkType::monitor_type(), monitor_count()),
+      "monitors");
+  }
+
+ public:
+  SharkMonitor* monitor(int index) const
+  {
+    return monitor(LLVMValue::jint_constant(index));
+  }
+  SharkMonitor* monitor(llvm::Value* index) const;
+
+ private:
+  llvm::Value* CreateBuildFrame();
+
+  // OopMap support
+ public:
+  // Every time a new, distinct pc is required, an extra byte is
+  // emitted into the codebuffer
+  int code_offset() const
+  {
+    int offset = masm()->offset();
+    masm()->advance(1); // keeps PCs unique
+    return offset;
+  }
+
+ private:
+  int _extended_frame_size;
+  int _stack_slots_offset;
+  int _monitors_slots_offset;
+  int _exception_slot_offset;
+  int _method_slot_offset;
+  int _pc_slot_offset;
+  int _locals_slots_offset;
+
+ public:
+  int extended_frame_size() const
+  {
+    return _extended_frame_size;
+  }
+  int oopmap_frame_size() const
+  {
+    return extended_frame_size() - arg_size();
+  }
+  int stack_slots_offset() const
+  {
+    return _stack_slots_offset;
+  }
+  int monitors_slots_offset() const
+  {
+    return _monitors_slots_offset;
+  }
+  int exception_slot_offset() const
+  {
+    return _exception_slot_offset;
+  }
+  int method_slot_offset() const
+  {
+    return _method_slot_offset;
+  }
+  int pc_slot_offset() const
+  {
+    return _pc_slot_offset;
+  }
+  int locals_slots_offset() const
+  {
+    return _locals_slots_offset;
+  }
+
+  // VM interface
+ private:
+  llvm::StoreInst* CreateStoreLastJavaSP(llvm::Value* value) const
+  {
+    return builder()->CreateStore(
+      value,
+      builder()->CreateAddressOfStructEntry(
+        thread(), JavaThread::last_Java_sp_offset(),
+        llvm::PointerType::getUnqual(SharkType::intptr_type()),
+        "last_Java_sp_addr"));
+  }
+
+ public:
+  void set_last_Java_frame()
+  {
+    CreateStoreLastJavaSP(CreateLoadZeroFramePointer());
+  }
+  void reset_last_Java_frame()
+  {
+    CreateStoreLastJavaSP(LLVMValue::intptr_constant(0));
+  }
+
+ public:
+  llvm::LoadInst* CreateGetVMResult() const
+  {
+    llvm::Value *addr = builder()->CreateAddressOfStructEntry(
+      thread(), JavaThread::vm_result_offset(),
+      llvm::PointerType::getUnqual(SharkType::jobject_type()),
+      "vm_result_addr");
+    llvm::LoadInst *result = builder()->CreateLoad(addr, "vm_result");
+    builder()->CreateStore(LLVMValue::null(), addr);
+    return result;
+  }
+
+ public:
+  llvm::Value* pending_exception_address() const
+  {
+    return builder()->CreateAddressOfStructEntry(
+      thread(), Thread::pending_exception_offset(),
+      llvm::PointerType::getUnqual(SharkType::jobject_type()),
+      "pending_exception_addr");
+  }
+  llvm::LoadInst* CreateGetPendingException() const
+  {
+    llvm::Value *addr = pending_exception_address();
+    llvm::LoadInst *result = builder()->CreateLoad(addr, "pending_exception");
+    builder()->CreateStore(LLVMValue::null(), addr);
+    return result;
+  }
+
+  // Deferred zero checks
+ public:
+  void add_deferred_zero_check(SharkTopLevelBlock* block,
+                               SharkValue*         value);
+
+ private:
+  void do_deferred_zero_checks();
+};
